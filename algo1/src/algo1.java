5bd803062754c4e26652bec1b0ba0a4b71ea1333
PTC\odsptc13 <dmitry.zvarich@gmail.com>
2017-03-16 17:59:16 +0200

    test method created

--- algo1/src/algo1.java
+++ algo1/src/algo1.java
@@ -2,26 +2,47 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 /**
  * Created by odsptc13 on 3/16/2017.
  */
 public class algo1 {
     public static void main(String[] args) {
-        List<Integer> iii = Arrays.asList(1,4,2,7,3,87);
-        Integer[] ar = {1,4,2,7,3,87};
-        String[] ar2 = {"a1","a4","a2","a7","a3","a87"};
+        List<Integer> ar = Arrays.asList(1,4,2,7,3,87);
+        List<String> ar2 = Arrays.asList("a1","a4","a2","a7","a3","a87");
 
-        test(algo1::search,ar,5,-11);
-
+        test(algo1::search,ar,5,1);
+        test(algo1::search,ar,5,-1);
         test(algo1::search,ar2,"a22", 2);
     }
 
-    private static <T> int search(T[] ar, T key)
+
+    private static <T> int binary_search(List<T> ar, T key)
     {
-        for(int i = 0; i < ar.length; i++)
+        assert ar.stream().sorted().collect(Collectors.toList()).equals(ar);
+
+        if()
+
+        int m = ar.size() / 2;
+        //[0, s] = [0, m] U [m, s]
+
+        int rl = binary_search();
+        if(rl != -1)
         {
-            if(ar[i] == key)
+            return rl;
+        }
+
+        return binary_search();
+
+
+    }
+
+    private static <T> int search(List<T> ar, T key)
+    {
+        for(int i = 0; i < ar.size(); i++)
+        {
+            if(ar.get(i) == key)
             {
                 return i;
             }
@@ -30,13 +51,15 @@ public class algo1 {
         return -1;
     }
 
-
-
-    private static <T> void test(BiFunction<T[], T, Integer> function, T[] ar, T arg, Integer exp)
+    private static <T> void test(BiFunction<List<T>, T, Integer> function, List<T> ar, T arg, Integer exp)
     {
         if(function.apply(ar, arg) != exp)
         {
             System.out.println("failed");
+        }
+        else
+        {
+            System.out.println("passed");
         }
     }
 
